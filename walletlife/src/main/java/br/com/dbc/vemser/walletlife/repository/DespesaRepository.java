12b19c31b16e4ea1849ae0f1ab10cba273817147
DiogoBruno06 <diogobruno06@outlook.com>
2023-08-15 14:53:01 -0300

    fix:DespesaService

--- walletlife/src/main/java/br/com/dbc/vemser/walletlife/repository/DespesaRepository.java
+++ walletlife/src/main/java/br/com/dbc/vemser/walletlife/repository/DespesaRepository.java
@@ -4,7 +4,9 @@ import br.com.dbc.vemser.walletlife.enumerators.TipoDespesaEReceita;
 import br.com.dbc.vemser.walletlife.exceptions.BancoDeDadosException;
 import br.com.dbc.vemser.walletlife.modelos.Despesa;
 import br.com.dbc.vemser.walletlife.modelos.Investimento;
+import br.com.dbc.vemser.walletlife.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Repository
 public interface DespesaRepository extends JpaRepository<Despesa,Integer> {
-//    List<Despesa> findAllBy
-//    List<Despesa> listDespesaListByIdFK(Integer idFk);
+   public List<Despesa> findByUsuario(Usuario usuario);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM DESPESA WHERE ID_USUARIO = :idUsuario")
+    List<Despesa> listDespesaListByIdUsuario(Integer idUsuario);
 }
