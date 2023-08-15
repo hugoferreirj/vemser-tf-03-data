312b9361323177143ee85b1654c1bc8d9155302a
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-08-15 17:15:33 -0300

    comentando testes

--- src/test/java/br/com/dbc/vemser/walletlife/WalletlifeApplicationTests.java
+++ src/test/java/br/com/dbc/vemser/walletlife/WalletlifeApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.dbc.vemser.walletlife;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WalletlifeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.dbc.vemser.walletlife;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WalletlifeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
